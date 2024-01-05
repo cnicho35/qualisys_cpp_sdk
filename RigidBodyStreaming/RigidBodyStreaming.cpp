3ab784c3c3c095e18d643c0b65c90228b12de9b2
cnichols <casey.nichols@nrel.gov>
2024-01-05 10:10:51 -0700

    added camera time

--- RigidBodyStreaming/RigidBodyStreaming.cpp
+++ RigidBodyStreaming/RigidBodyStreaming.cpp
@@ -1,5 +1,6 @@
 #include "RTProtocol.h"
 #include "RTPacket.h"
+#include <iostream>
 
 #ifdef _WIN32
 #define sleep Sleep
@@ -13,37 +14,37 @@ int main(int argc, char **argv)
     {
         CRTProtocol rtProtocol;
 
-        //Example code for how to use discovery calls.
-        //if (rtProtocol.DiscoverRTServer(4534, false))
+        // Example code for how to use discovery calls.
+        // if (rtProtocol.DiscoverRTServer(4534, false))
         //{
-        //    sleep(1);
-        //    const auto numberOfResponses = rtProtocol.GetNumberOfDiscoverResponses();
-        //    for (auto index = 0; index < numberOfResponses; index++)
-        //    {
-        //        unsigned int addr;
-        //        unsigned short basePort;
-        //        std::string message;
-        //        if (rtProtocol.GetDiscoverResponse(index, addr, basePort, message))
-        //        {
-        //            printf("%2d - %d.%d.%d.%d:%d\t- %s\n", index, 0xff & addr, 0xff & (addr >> 8), 0xff & (addr >> 16), 0xff & (addr >> 24), basePort, message.c_str());
-        //        }
-        //    }
-        //}
-        //else
+        //     sleep(1);
+        //     const auto numberOfResponses = rtProtocol.GetNumberOfDiscoverResponses();
+        //     for (auto index = 0; index < numberOfResponses; index++)
+        //     {
+        //         unsigned int addr;
+        //         unsigned short basePort;
+        //         std::string message;
+        //         if (rtProtocol.GetDiscoverResponse(index, addr, basePort, message))
+        //         {
+        //             printf("%2d - %d.%d.%d.%d:%d\t- %s\n", index, 0xff & addr, 0xff & (addr >> 8), 0xff & (addr >> 16), 0xff & (addr >> 24), basePort, message.c_str());
+        //         }
+        //     }
+        // }
+        // else
         //{
-        //    printf("%s", rtProtocol.GetErrorString());
-        //}
+        //     printf("%s", rtProtocol.GetErrorString());
+        // }
 
-        const char           serverAddr[] = "192.168.254.001";
+        const char serverAddr[] = "192.168.254.001";
         const unsigned short basePort = 22222;
-        const int            majorVersion = 1;
-        const int            minorVersion = 19;
-        const bool           bigEndian = false;
+        const int majorVersion = 1;
+        const int minorVersion = 19;
+        const bool bigEndian = false;
 
         bool dataAvailable = false;
         bool streamFrames = false;
         unsigned short udpPort = 6734;
-        unsigned long long timestampOut;
+        unsigned long long cameraTime;
         while (true)
         {
             if (!rtProtocol.Connected())
@@ -68,7 +69,7 @@ int main(int argc, char **argv)
 
             if (!streamFrames)
             {
-                if (!rtProtocol.StreamFrames(CRTProtocol::RateAllFrames, 0, udpPort, NULL, CRTProtocol::cComponent6d))
+                if (!rtProtocol.StreamFrames(CRTProtocol::RateAllFrames, 0, udpPort, NULL, {"2,9"}))
                 {
                     printf("rtProtocol.StreamFrames: %s\n\n", rtProtocol.GetErrorString());
                     sleep(1);
@@ -88,16 +89,20 @@ int main(int argc, char **argv)
                     float fX, fY, fZ;
                     float rotationMatrix[9];
 
-                    CRTPacket* rtPacket = rtProtocol.GetRTPacket();
+                    CRTPacket *rtPacket = rtProtocol.GetRTPacket();
 
                     printf("Frame %d\n", rtPacket->GetFrameNumber());
                     printf("======================================================================================================================\n");
 
+                    rtPacket->GetTimecodeCameraTime(cameraTime);
+                    std::cout << "Camera Time: " << cameraTime << std::endl;
+
                     for (unsigned int i = 0; i < rtPacket->Get6DOFBodyCount(); i++)
                     {
+
                         if (rtPacket->Get6DOFBody(i, fX, fY, fZ, rotationMatrix))
                         {
-                            const char* pTmpStr = rtProtocol.Get6DOFBodyName(i);
+                            const char *pTmpStr = rtProtocol.Get6DOFBodyName(i);
                             if (pTmpStr)
                             {
                                 printf("%-12s ", pTmpStr);
@@ -106,21 +111,23 @@ int main(int argc, char **argv)
                             {
                                 printf("Unknown     ");
                             }
-                            printf("Pos: %9.3f %9.3f %9.3f    Rot: %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f  \n",
-                                fX, fY, fZ, rotationMatrix[0], rotationMatrix[1], rotationMatrix[2],
-                                rotationMatrix[3], rotationMatrix[4], rotationMatrix[5], rotationMatrix[6], rotationMatrix[7], rotationMatrix[8]);
+                            printf("Pos: %9.3f %9.3f %9.3f    Rot: %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n",
+                                   fX, fY, fZ, rotationMatrix[0], rotationMatrix[1], rotationMatrix[2],
+                                   rotationMatrix[3], rotationMatrix[4], rotationMatrix[5], rotationMatrix[6], rotationMatrix[7], rotationMatrix[8]);
                         }
-                        
                     }
                     printf("\n");
                 }
             }
-            break;
+            else
+            {
+                std::cout << "Error if statement" << std::endl;
+            }
         }
         rtProtocol.StopCapture();
         rtProtocol.Disconnect();
     }
-    catch (std::exception& e)
+    catch (std::exception &e)
     {
         printf("%s\n", e.what());
     }
