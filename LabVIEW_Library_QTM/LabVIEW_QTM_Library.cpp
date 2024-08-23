ccc37b88b4fbcc3dc121f37703d47e5c61993dde
cnichols <casey.nichols@nrel.gov>
2024-08-23 09:25:24 -0600

    changes for UMaine test

--- LabVIEW_Library_QTM/LabVIEW_QTM_Library.cpp
+++ LabVIEW_Library_QTM/LabVIEW_QTM_Library.cpp
@@ -10,6 +10,8 @@ bool dataAvailable = false;
 bool streamFrames = false;
 unsigned short udpPort = 6734;
 
+int frameNum = 0;
+
 CRTPacket::EPacketType packetType;
 
 std::stringstream initString;
@@ -19,22 +21,21 @@ unsigned long long cameraTime;
 int32_t failedInit;
 
 const int BUFFER_SIZE = 512;
-char* cString = new char[BUFFER_SIZE];
+char *cString = new char[BUFFER_SIZE];
 std::string result;
 
-int32_t initQTM(const char * ipAddress, unsigned short  * qtmPort)
+int32_t initQTM(const char *ipAddress, unsigned short *qtmPort)
 {
     const char *serverAddr = ipAddress;
     const unsigned short basePort = *qtmPort;
-    failedInit= 0;
-
+    failedInit = 0;
 
     if (!rtProtocol.Connected())
     {
         if (!rtProtocol.Connect(serverAddr, basePort, &udpPort, majorVersion, minorVersion, bigEndian))
         {
 
-            failedInit= 1;
+            failedInit = 1;
         }
     }
 
@@ -42,7 +43,7 @@ int32_t initQTM(const char * ipAddress, unsigned short  * qtmPort)
     {
         if (!rtProtocol.Read6DOFSettings(dataAvailable))
         {
-            failedInit= 1;
+            failedInit = 1;
         }
     }
 
@@ -50,15 +51,15 @@ int32_t initQTM(const char * ipAddress, unsigned short  * qtmPort)
     {
         if (!rtProtocol.StreamFrames(CRTProtocol::RateAllFrames, 0, udpPort, NULL, "6d timecode"))
         {
-            failedInit= 1;
+            failedInit = 1;
         }
     }
-     return failedInit;
+    return failedInit;
 }
-char* getData()
+char *getData()
 {
-    dataString.str("");  // Clear the stringstream
-    result.clear();      // Clear the result string
+    dataString.str(""); // Clear the stringstream
+    result.clear();     // Clear the result string
     strncpy(cString, "", BUFFER_SIZE - 1);
 
     dataString << std::fixed;
@@ -70,10 +71,8 @@ char* getData()
             float fX, fY, fZ;
             float rotationMatrix[9];
 
-            CRTPacket* rtPacket = rtProtocol.GetRTPacket();
-
-            printf("Frame %d\n", rtPacket->GetFrameNumber());
-            printf("======================================================================================================================\n");
+            CRTPacket *rtPacket = rtProtocol.GetRTPacket();
+            frameNum = rtPacket->GetFrameNumber();
 
             rtPacket->GetTimecodeCameraTime(cameraTime);
 
@@ -81,18 +80,21 @@ char* getData()
             {
                 if (rtPacket->Get6DOFBody(i, fX, fY, fZ, rotationMatrix))
                 {
-                    dataString << fX << "," << fY << "," << fZ << "," << rotationMatrix[0] << "," << rotationMatrix[1] << "," << rotationMatrix[2] << "," << rotationMatrix[3] << "," << rotationMatrix[4] << "," << rotationMatrix[5] << "," << rotationMatrix[6] << "," << rotationMatrix[7] << "," << rotationMatrix[8] << "," << cameraTime;
+                    dataString << frameNum << fX << "," << fY << "," << fZ << "," << rotationMatrix[0] << "," << rotationMatrix[1] << "," << rotationMatrix[2] << "," << rotationMatrix[3] << "," << rotationMatrix[4] << "," << rotationMatrix[5] << "," << rotationMatrix[6] << "," << rotationMatrix[7] << "," << rotationMatrix[8] << "," << cameraTime;
                 }
                 else
                 {
-                    dataString << "ERROR";
+                    dataString << "ERROR:6D";
                 }
-
-                result = dataString.str();
-                strncpy(cString, result.c_str(), BUFFER_SIZE - 1);
             }
         }
     }
+    else
+    {
+        dataString << "ERROR:Rec";
+    }
+    result = dataString.str();
+    strncpy(cString, result.c_str(), BUFFER_SIZE - 1);
     return cString;
 }
 void closeQTM()
